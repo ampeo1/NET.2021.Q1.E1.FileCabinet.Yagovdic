c20cf2ca366b0e4a5b03680dc586602cc3ab01bd
ampeo <ampeo@yandex.ru>
2021-03-21 00:35:00 +0300

    Add FileCabinetService.cs file.

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+
+        public static int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+
+        public static FileCabinetRecord[] GetRecords()
+        {
+            // TODO: добавьте реализацию метода
+            return Array.Empty<FileCabinetRecord>();
+        }
+
+        public static int GetStat()
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+    }
+}
