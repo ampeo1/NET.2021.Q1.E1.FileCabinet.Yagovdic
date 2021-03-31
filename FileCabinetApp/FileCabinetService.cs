ee2753f0d29fc367a8c1f236cdcd68b851375f01
ampeo <ampeo@yandex.ru>
2021-03-31 16:19:13 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -12,9 +12,24 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, char access)
         {
-            if (firstName is null || lastName is null)
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
             {
                 throw new ArgumentNullException($"{nameof(firstName)}, {nameof(lastName)}");
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60 || lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(firstName)} or {nameof(lastName)} length is less than 2 or greater than 60", $"{nameof(firstName)}, {nameof(lastName)}");
+            }
+
+            if (dateOfBirth < new DateTime(01, 01, 1950) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException($"{nameof(dateOfBirth)} is less than 01-jan-1950 or greater than now");
+            }
+
+            if (access < 'A' || access > 'G')
+            {
+                throw new ArgumentException($"{nameof(access)} does not contains [A, B, C, D, E, F, G]");
             }
 
             short age = (short)(DateTime.Now.Year - dateOfBirth.Year);
