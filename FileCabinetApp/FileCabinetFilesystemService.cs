ba5cc02a6b5367f623c0cde5ce4825d20f517a7c
ampeo <ampeo@yandex.ru>
2021-04-19 21:14:20 +0300

    Implement CreateRecord for FileCabinetFilesystemService

--- FileCabinetApp/FileCabinetFilesystemService.cs
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -12,8 +12,11 @@ namespace FileCabinetApp
     /// </summary>
     public class FileCabinetFilesystemService : IFileCabinetService
     {
+        private const short SizeRecord = 276;
+        private const byte SizeStringProperty = 120;
         private readonly FileStream fileStream;
         private readonly IRecordValidator validator;
+        private int id;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FileCabinetFilesystemService"/> class.
@@ -24,12 +27,47 @@ namespace FileCabinetApp
         {
             this.fileStream = fileStream ?? throw new ArgumentNullException(nameof(fileStream));
             this.validator = validator ?? throw new ArgumentNullException(nameof(validator));
+            this.id = (int)fileStream.Length / SizeRecord;
         }
 
         /// <inheritdoc/>
         public int CreateRecord(DataRecord dataRecord)
         {
-            throw new NotImplementedException();
+            if (dataRecord is null)
+            {
+                throw new ArgumentNullException(nameof(dataRecord));
+            }
+
+            List<byte> data = new List<byte>();
+            dataRecord.Id = ++this.id;
+            FileCabinetRecord record = this.Create(dataRecord);
+
+            data.AddRange(BitConverter.GetBytes(record.Id));
+
+            byte[] dataForString = new byte[SizeStringProperty];
+            Encoding.Default.GetBytes(record.FirstName, 0, record.FirstName.Length, dataForString, 0);
+            data.AddRange(dataForString);
+
+            dataForString = new byte[SizeStringProperty];
+            Encoding.Default.GetBytes(record.LastName, 0, record.FirstName.Length, dataForString, 0);
+            data.AddRange(dataForString);
+
+            data.AddRange(BitConverter.GetBytes(record.DateOfBirth.Year));
+            data.AddRange(BitConverter.GetBytes(record.DateOfBirth.Month));
+            data.AddRange(BitConverter.GetBytes(record.DateOfBirth.Day));
+
+            data.AddRange(BitConverter.GetBytes(record.Access));
+
+            data.AddRange(BitConverter.GetBytes(record.Age));
+            int[] bits = decimal.GetBits(record.Salary);
+            foreach (int bit in bits)
+            {
+                data.AddRange(BitConverter.GetBytes(bit));
+            }
+
+            this.fileStream.Position = this.fileStream.Length;
+            this.fileStream.Write(data.ToArray(), 0, data.Count);
+            return 0;
         }
 
         /// <inheritdoc/>
@@ -77,13 +115,42 @@ namespace FileCabinetApp
         /// <inheritdoc/>
         public IRecordValidator GetValidator()
         {
-            throw new NotImplementedException();
+            return this.validator;
         }
 
         /// <inheritdoc/>
         public FileCabinetServiceSnapshot MakeSnapshot()
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates record.
+        /// </summary>
+        /// <param name="dataRecord">Record data.</param>
+        /// <returns>Record.</returns>
+        private FileCabinetRecord Create(DataRecord dataRecord)
+        {
+            this.validator.ValidateParameters(dataRecord);
+
+            short age = (short)(DateTime.Now.Year - dataRecord.DateOfBirth.Year);
+            if (dataRecord.DateOfBirth > DateTime.Now.AddYears(-age))
+            {
+                age--;
+            }
+
+            var record = new FileCabinetRecord
+            {
+                Id = dataRecord.Id,
+                FirstName = dataRecord.FirstName,
+                LastName = dataRecord.LastName,
+                DateOfBirth = dataRecord.DateOfBirth,
+                Access = dataRecord.Access,
+                Salary = dataRecord.Salary,
+                Age = age,
+            };
+
+            return record;
         }
     }
 }
